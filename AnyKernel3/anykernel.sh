3a0838ae9ac10517fb0023e209afa56157b89e5b
Zarathos30 <vale.rigni@gmail.com>
2026-08-10 22:20:19 +0200

    AK3: switch to proven 20231020 base (GhostKernel), keep arm64 busybox/magiskboot
    Flash on onyx failed with the 20260704 base: it reads uppercase shell
    variables (BLOCK, IS_SLOT_DEVICE, ...) while anykernel.sh set the legacy
    lowercase ones, leaving BLOCK empty -> 'Unable to determine partition.
    Aborting...'. Adopt the internally consistent 20231020 base used by the
    GKI GhostKernel build (block=boot name-based detection with slot), keeping
    the arm64 busybox/magiskboot, and bring in httools_static/lptools_static/
    snapshotupdater_static/fec/magiskpolicy (unused on onyx: boot/dtbo are
    raw by-name partitions).

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -1,8 +1,8 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
 kernel.string=GhostKernelOSS by Zarathos30
 do.devicecheck=1
@@ -10,7 +10,6 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-do.check_boot_version=1
 device.name1=onyx
 device.name2=onyx_in
 device.name3=onyx_global
@@ -18,29 +17,29 @@ device.name4=aliothin
 device.name5=
 supported.versions=16
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-dtbo_block=/dev/block/bootdevice/by-name/dtbo;
-is_slot_device=auto;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-no_block_display=1;
+
+### AnyKernel install
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh
 
-## AnyKernel functions
-# GhostKernelOSS banner
-ui_print ' ';
-ui_print '   ___ _           _   _  __                 _  ___  ___ ___ ';
-ui_print '  / __| |_  ___ __| |_| |/ /___ _ _ _ _  ___| |/ _ \/ __/ __|';
-ui_print '| (_ | '\'' \/ _ (_-<  _| | '\'' </ -_) '\''_| '\'' \/ -_) | (_) \__ \__ \';
-ui_print '  \___|_||_\___/__/\__|_|\_\___|_| |_||_\___|_|\___/|___/___/';
-ui_print ' ';
-ui_print 'GhostKernelOSS by Zarathos30 | POCO F7 (onyx) | SM8735';
-ui_print ' ';
+ui_print " "
+ui_print "   ___ _           _   _  __                 _  ___  ___ ___ "
+ui_print "  / __| |_  ___ __| |_| |/ /___ _ _ _ _  ___| |/ _ \/ __/ __|"
+ui_print "| (_ | '\'' \/ _ (_-<  _| | '\'' </ -_) '\''_| '\'' \/ -_) | (_) \__ \__ \";
+ui_print "  \___|_||_\___/__/\__|_|\_\___|_| |_||_\___|_|\___/|___/___/";
+ui_print " "
+ui_print "GhostKernelOSS by Zarathos30 | POCO F7 (onyx) | SM8735"
+ui_print " "
 
 # dump boot and extract ramdisk
 dump_boot;
